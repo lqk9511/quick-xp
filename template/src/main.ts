dd48ba556e211342052924645a73cf68f98ce367
xiaojianhua <xiaojianhua@xiaoheiban.cn>
2019-09-17 15:12:26 +0800

    添加声明文件 修改晓程序appId环境配置

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import xp from '@heibanfe/xp-sdk'
 
 Vue.config.productionTip = false
+//
+const env = process.env.NODE_ENV === 'development'
+xp.config({
+  baseURL: '/api',
+  storageKey: process.env.VUE_APP_APP_ID,
+  isDevelopment: env,
+  router
+})
 
 new Vue({
   router,
