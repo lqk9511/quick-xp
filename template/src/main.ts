0d2da7954b1bd31d0f1323c96a0860eff63837fe
xiaojianhua <xiaojianhua@xiaoheiban.cn>
2019-09-24 13:13:22 +0800

    添加sentry配置,  修改goctl路径

--- template/src/main.ts
+++ template/src/main.ts
@@ -2,18 +2,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import xp from '@heibanfe/xp-sdk'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
 Vue.config.productionTip = false
-//
 const env = process.env.NODE_ENV === 'development'
+
 xp.config({
-  baseURL: '/api',
+  baseURL: `${env ? '' : process.env.VUE_APP_API_DOMAIN}/api`,
   storageKey: process.env.VUE_APP_APP_ID,
   isDevelopment: env,
   router
-})
+}).then(() => {
+  new Vue({
+    router,
+    created() {
+      if (!env) {
+        xp.getAppInfo().then(({ environment: appEnv }) => {
+          const isUat = !appEnv || appEnv === 'unknow' || appEnv === 'debug' || appEnv === 'development'
+          const dsn = isUat ? 'https://4801b1499eb04846915dc3adae986021@sentry.xiaoheiban.cn/5' : 'https://46b5c12b52084c71a5930c6fb2d1fdd6@sentry.xiaoheiban.cn/8'
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+          Sentry.init({
+            dsn,
+            environment: process.env.VUE_APP_APP_ID,
+            integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+            release: process.env.VUE_APP_APP_ID
+          })
+        })
+      }
+    },
+    render: h => h(App)
+  }).$mount('#app')
+})
--- template/vue.config.js
+++ template/vue.config.js
@@ -1,4 +1,5 @@
 const publicPath = process.env.NODE_ENV !== 'development' ? '././' : ''
+const SentryPlugin = require('@sentry/webpack-plugin')
 const GenerateAssetPlugin = require('generate-asset-webpack-plugin')
 const config = require('./config.json')
 const createJson = compilation => {
@@ -36,5 +37,14 @@ module.exports = {
       }
     }))
     config.module.rules[1].use.push('image-webpack-loader')
+    config.plugins.push(
+      new SentryPlugin({
+        include: config.output.path + '/js',
+        environment: process.env.VUE_APP_APP_ID,
+        ignore: ['node_modules', 'babel.config.js', 'vue.config.js'],
+        release: process.env.VUE_APP_APP_ID,
+        configFile: './config/sentry.properties'
+      })
+    )
   }
 }
